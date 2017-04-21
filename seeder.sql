a2d47cbb4ab11d2fc05b78269899103e9836e458
russotx <russotx@gmail.com>
2017-04-21 00:25:51 -0500

    added seeder

---
+++ seeder.sql
@@ -0,0 +1,29 @@
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ('plaid socks','menswear',13.50,300);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ('Samsung LED TV','electronics',1200.00,20);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ('Dyson Vacuum','appliances',520.35,9);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ('Samsung Washing Machine','appliances',1500.24,3);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ('v-neck shirts','menswear',8.00,200);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ('size 3 diapers','baby',36.20,1000);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ('toothpaste','bath and body',4.50,150);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ('Listerene mouthwash','bath and body',6.75,20);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ('Google Home','electronics',89.00,56);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ('baby wipes','baby',12.30,1200);
